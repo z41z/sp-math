bae0e4014fd9278c7a49a71873eb28d5f67059fa
z41z <just4iu@sina.com>
2018-12-13 09:17:41 +0800

    add point rotate by degree function

---
+++ src/point/rotateByDegree.js
@@ -0,0 +1,16 @@
+/**
+ * x,y旋转后坐标计算
+ * @param {Number} _x 
+ * @param {Number} _y 
+ * @param {Number} degree 
+ */
+const rotateByDegree = (_x = 0, _y = 0, degree = 0) => {
+  let x = _x * cos(degree) - _y * sin(degree)
+  let y = _y * cos(degree) + _x * sin(degree)
+  return {
+    x,
+    y
+  }
+};
+
+module.exports.default = module.exports = rotateByDegree;
